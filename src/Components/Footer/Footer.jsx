2e2dffe7490e0dd00a1b3c881e07e8d212e821de
WillDz90 <will.diazor@gmail.com>
2022-08-01 18:13:06 -0500

    Fix the footer bug

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -10,22 +10,23 @@ export default function Footer() {
 
   return (
     <footer className="body">
-      <div className="container mt-5">
-        <div className="row">
-          <div className="col-md-5">
-            <i className="fa fa-road"></i>
-            <NavLink to={"/home"} className={"footer-link"}>
-              <img src={logo} alt="Rebirth"></img>
-            </NavLink>
-            <div className="sub-title">
-              <p>
-                Pet adoption network, to give
-                <br /> them a new life opportunity.
-              </p>
+      <div className="upperContainer">
+        <div className="containerFooter mt-5">
+          
+            <div>
+              <NavLink to={"/home"} className={"footer-link"}>
+                  <img src={logo} alt="Rebirth"></img>
+                </NavLink>
+                <div className="sub-title">
+                  <p>
+                    Pet adoption network, to give
+                    <br /> them a new life opportunity.
+                  </p>
+                </div>
             </div>
 
-            <div className="row-lists">
-              <div className="col-6">
+
+            <div>
                 <ul className="list-unstyled">
                   Company
                   <li className="nav-item">
@@ -41,62 +42,66 @@ export default function Footer() {
                     <a href={user!==null ? "/donations" : "/login"}>Donate</a>
                   </li>
                 </ul>
-              </div>
-              <div className="col-6">
-                <ul className="list-unstyled">
-                  {" "}
-                  Services
-                  <li className="nav-item">
-                    <NavLink to={"/faqs"}>F. A. Q. s</NavLink>
-                  </li>
-                  <li className="nav-item">
-                    <a href={user!==null ? "/favorites" : "/login"}>My Favorites</a>
-                  </li>
-                  <li className="nav-item">
-                    <a href=" ">My account</a>
-                  </li>
-                  <li className="nav-item">
-                    <NavLink to={user!==null ? "/profile" : "/login"}>Terms & Conditions</NavLink>
-                  </li>
-                </ul>
-              </div>
-              <div className="newsletter">
-                <ul className="list-unstyled">
-                  {" "}
-                  Get in Touch
-                  <li className="nav-item">
-                    <div className="paragraph">
-                      Subscribe to our weekly <br /> newsletter and receive
-                      <br />
-                      updates via email.
-                    </div>
-                  </li>
-                  <li className="nav-item">
-                    <a href=" " className="nav-link pl-0">
-                      <img src={socials} alt="Socials" className="socials" />
-                    </a>
-                  </li>
-                </ul>
-              </div>
             </div>
-          </div>
-        </div>
-        <div className="lower">
-          <h3 className="rights">All Rights Reserved @ Team 13</h3>
-          <ul className="footer-bottom">
-            <li className="li-bottom">
-              <span className="a-bottom">
-                <NavLink to={"/terms"}>Terms & Conditions</NavLink>
-              </span>
-            </li>
-            <li className="li-bottom">
-              <span className="a-bottom">
-                <NavLink to={"/privacy"}>Privacy & Policy</NavLink>
-              </span>
-            </li>
-          </ul>
+
+
+            <div>
+              <ul className="list-unstyled">
+                    Services
+                    <li className="nav-item">
+                      <NavLink to={"/faqs"}>F. A. Q. s</NavLink>
+                    </li>
+                    <li className="nav-item">
+                      <a href={user!==null ? "/favorites" : "/login"}>My Favorites</a>
+                    </li>
+                    <li className="nav-item">
+                      <a href=" ">My account</a>
+                    </li>
+                    <li className="nav-item">
+                      <NavLink to={user!==null ? "/profile" : "/login"}>Terms & Conditions</NavLink>
+                    </li>
+              </ul>
+            </div>
+
+
+            <div>
+              <ul className="list-unstyled">
+                Get in Touch
+                <li className="nav-item">
+                  <div className="paragraph">
+                    Subscribe to our weekly <br /> newsletter and receive
+                    <br />
+                    updates via email.
+                  </div>
+                </li>
+                <li className="nav-item">
+                  <a href=" " className="nav-link pl-0">
+                    <img src={socials} alt="Socials" className="socials" />
+                  </a>
+                </li>
+              </ul>
+            </div>
         </div>
       </div>
+
+      <div className="downContainer">
+        <div className="lower">
+            <h3 className="rights">All Rights Reserved @ Team 13</h3>
+            <ul className="footer-bottom">
+              <li className="li-bottom">
+                <span className="a-bottom">
+                  <NavLink to={"/terms"}>Terms & Conditions</NavLink>
+                </span>
+              </li>
+              <li className="li-bottom">
+                <span className="a-bottom">
+                  <NavLink to={"/privacy"}>Privacy & Policy</NavLink>
+                </span>
+              </li>
+            </ul>
+        </div>
+      </div>
+
     </footer>
   );
 }
