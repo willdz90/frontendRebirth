62e09f7961891d9a182317fea3409bb01d03ccbc
WillDz90 <will.diazor@gmail.com>
2022-08-11 01:29:01 -0500

    Act v2.1.5

--- src/Pages/Details.jsx
+++ src/Pages/Details.jsx
@@ -52,6 +52,8 @@ function Details() {
     var mail = user.mail;
   }
 
+  const [ cargando, setCargando ] = useState(true);
+
   useEffect(() => {
     dispatch(getDetails(id));
     if (user) {
@@ -77,89 +79,94 @@ function Details() {
       <Navbar />
       <Container>
         <Header2 />
-        <br />
-        <div className="dtl-card">
-          <div className="dtl-cardLeft">
-            <h3 className="title">{name}</h3>
-            <h4 className="breed">{race}</h4>
-            <h5 className="age">{age}&nbsp;years</h5>
-            <span className="petlocation">{location}</span>
-            {user && user.isAdmin === true ? (
-              <div className="mail">
-                <Link to={"/users"} className="mail">
-                  <span>User: {userMail}</span>
-                </Link>
+        {
+          cargando ? null :
+          <>
+            <br />
+            <div className="dtl-card">
+              <div className="dtl-cardLeft">
+                <h3 className="title">{name}</h3>
+                <h4 className="breed">{race}</h4>
+                <h5 className="age">{age}&nbsp;years</h5>
+                <span className="petlocation">{location}</span>
+                {user && user.isAdmin === true ? (
+                  <div className="mail">
+                    <Link to={"/users"} className="mail">
+                      <span>User: {userMail}</span>
+                    </Link>
+                  </div>
+                ) : null}
+                <br />
+                <div className="story-dtl">
+                  <h5 className="txt-dscp">{description}</h5>
+                </div>
               </div>
-            ) : null}
-            <br />
-            <div className="story-dtl">
-              <h5 className="txt-dscp">{description}</h5>
-            </div>
-          </div>
-          <div className="dtl-cardCenter">
-            <h3 className="disp">AVAILABLE</h3>
+              <div className="dtl-cardCenter">
+                <h3 className="disp">AVAILABLE</h3>
 
-            <div className="txt-description">
-              <span>{gender}</span>
-              {gender === "male" ? (
-                <img src={male} alt="gender" />
-              ) : (
-                <img src={female} alt="gender" />
-              )}
-            </div>
+                <div className="txt-description">
+                  <span>{gender}</span>
+                  {gender === "male" ? (
+                    <img src={male} alt="gender" />
+                  ) : (
+                    <img src={female} alt="gender" />
+                  )}
+                </div>
 
-            <br />
+                <br />
 
-            <div className="txt-description">
-              <span>{size}</span>
-              {type === "cat" ? (
-                <img src={catIco} alt="type" />
-              ) : (
-                <img src={dogIco} alt="type" />
-              )}
-            </div>
+                <div className="txt-description">
+                  <span>{size}</span>
+                  {type === "cat" ? (
+                    <img src={catIco} alt="type" />
+                  ) : (
+                    <img src={dogIco} alt="type" />
+                  )}
+                </div>
 
-            {user && user.isAdmin === true ? null : mail === userMail ? null : (
-              <Link to={user !== null ? `/donations/${id}` : `/login`}>
-                <button className="a-btn">
-                  <span>Donate</span>
-                </button>
-              </Link>
-            )}
-            {user && user.isAdmin === true ? null : mail === userMail ? null : (
-              <Link to={user !== null ? `/adoption/${id}` : `/login`}>
-                <button className="b-btn">
-                  <span>Adopt me!</span>
-                </button>
-              </Link>
-            )}
-          </div>
-
-          <div className="dtl-cardRight">
-            <div className="img-dtl">
-              <div>
-                {user && user.isAdmin === true ? null : user &&
-                  mail === userMail ? null : favFilter &&
-                  favFilter.length !== 0 ? (
-                  <>
-                    <button
-                      className="a-btnFavEliminar"
-                      onClick={handleDeleteFav}
-                    />
-                  </>
-                ) : (
-                  <>
-                  {
-                    !user ? null : 
-                    <button className="a-btnFav" onClick={handleFavorite} />
-                  }
-                  </>
+                {user && user.isAdmin === true ? null : mail === userMail ? null : (
+                  <Link to={user !== null ? `/donations/${id}` : `/login`}>
+                    <button className="a-btn">
+                      <span>Donate</span>
+                    </button>
+                  </Link>
+                )}
+                {user && user.isAdmin === true ? null : mail === userMail ? null : (
+                  <Link to={user !== null ? `/adoption/${id}` : `/login`}>
+                    <button className="b-btn">
+                      <span>Adopt me!</span>
+                    </button>
+                  </Link>
                 )}
               </div>
-              <img src={image} alt="Pet" className="img" />
+
+              <div className="dtl-cardRight">
+                <div className="img-dtl">
+                  <div>
+                    {user && user.isAdmin === true ? null : user &&
+                      mail === userMail ? null : favFilter &&
+                      favFilter.length !== 0 ? (
+                      <>
+                        <button
+                          className="a-btnFavEliminar"
+                          onClick={handleDeleteFav}
+                        />
+                      </>
+                    ) : (
+                      <>
+                      {
+                        !user ? null : 
+                        <button className="a-btnFav" onClick={handleFavorite} />
+                      }
+                      </>
+                    )}
+                  </div>
+                  <img src={image} alt="Pet" className="img" />
+                </div>
+              </div>
             </div>
-          </div>
-        </div>
+          </>
+        }
       </Container>
       <Footer />
     </div>
